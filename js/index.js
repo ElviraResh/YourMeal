5cad505c8b1f7e32e7367a302ed4e113899bc9ef
Elvira Reshetnikova <83705191+ElviraResh@users.noreply.github.com>
2022-12-01 17:27:57 +0500

    create card, navigation list, api

--- js/index.js
+++ js/index.js
@@ -1,51 +1,26 @@
-const modalProduct = document.querySelector('.modal_product');
-const catalogList = document.querySelector('.catalog__list');
+import { createCardProduct } from "./createCardProduct.js";
+import { modalProduct, catalogList } from "./elements.js";
+import { navigationListController } from "./navigationListController.js";
+import { openModal } from "./openModal.js";
+import { renderListProduct } from "./renderListProduct.js";
 
-const product = {
+const burgerMax = {
   title: 'Бургер Макс',
   price: 10000,
   weight: 5000,
   calories: 15000,
   description: 'Огромный бургер, съешь сам или поделись с компанией',
   image: 'img/megaburger.jpg',
-  ingredients: ['Пшеничная булочка', 'Мега котлета из говядины','Много сыра', 'Листья салата', 'Чипотл']
+  ingredients: ['Пшеничная булочка', 'Мега котлета из говядины', 'Много сыра', 'Листья салата', 'Чипотл']
 };
-
-const modalProductTitle = document.querySelector('.modal-product__title');
-const modalProductImage = document.querySelector('.modal-product__image');
-const modalProductDescription = document.querySelector('.modal-product__description');
-const ingredientsList = document.querySelector('.ingredients__list');
-const ingredientsCalories = document.querySelector('.ingredients__calories');
-const modalProductPriceCount = document.querySelector('.modal-product__price-count');
-
-modalProductTitle.textContent = product.title;
-modalProductImage.textContent = product.image;
-
-ingredientsList.textContent = '';
-
-/*for (let i =0; i < product.ingredients.length; i++) {
-  const li = document.createElement('li');
-  li.classList.add('ingredients__item');
-  li.textContent = product.ingredients[i];
-  ingredientsList.append(li);
-}*/
-
-const ingredientsListItems = product.ingredients.map((item) => {
-  const li = document.createElement('li');
-  li.classList.add('ingredients__item');
-  li.textContent = item;
-  return li;
-});
-
-ingredientsList.append(...ingredientsListItems);
 
 catalogList.addEventListener('click', (event) => {
   const target = event.target;
 
   if (target.closest('.product__detail') || target.closest('.product__image')) {
-    modalProduct.classList.add('modal_open');
+    openModal(burgerMax);
   }
-  
+
 });
 
 modalProduct.addEventListener('click', (event) => {
@@ -54,4 +29,12 @@ modalProduct.addEventListener('click', (event) => {
   if (target.closest('.modal__close') || target === modalProduct) {
     modalProduct.classList.remove('modal_open');
   }
-})
+});
+
+const init = () => {
+  renderListProduct();
+  navigationListController();
+};
+
+init();
+
